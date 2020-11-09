424e3b430b53bd866353750f886441dadf35db1f
anindanityaS <anindanitya22@gmail.com>
2020-11-09 21:38:01 +0700

    tambah navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
+import {NavDropdown} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App(props) {
+  return(
+    <header>
+      <Navbar bg="dark" expand="lg" variant="dark">
+  <Navbar.Brand href="#home">DTS</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Menu</Nav.Link>
+      </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+    </header>
   );
 }
-
-export default App;
