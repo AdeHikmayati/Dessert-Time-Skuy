74c83ba026f4e3b33677491c1d2583427d56773c
Nivan Sugiantoro <nivanrs@gmail.com>
2020-11-10 08:36:28 +0700

    add contact form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
+class MyForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      mail: '',
+      message: '',
+    };
+  }
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
+  render() {
+    return (
+      <form>
+      <h1>Contact US</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        name='username'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter your mail:</p>
+      <input
+        type='email'
+        name='mail'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter your messege:</p>
+      <input
+        type='text'
+        name='message'
+        onChange={this.myChangeHandler}
+      />
+      </form>
+    );
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyForm/>
   );
 }
 
