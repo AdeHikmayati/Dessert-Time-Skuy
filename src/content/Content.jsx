8ed8300c1210869503b81d2c3c928f9e1a954e0c
AdeHikmayati <pabdillah27@gmail.com>
2020-12-17 14:05:07 +0700

    ganti API

--- src/content/Content.jsx
+++ src/content/Content.jsx
@@ -13,7 +13,7 @@ class Content extends Component{
     }
 
     getPostApi = () => {
-        axios.get('http://localhost:3004/posts')
+        axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert')
         .then((res) => {
              //console.log(res.data);
             this.setState({
